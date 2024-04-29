186462af56bf83cca6922b0ef716731bef8ec6cb
KarenAlessandra <20971247+KarenAlessandra@users.noreply.github.com>
2024-04-29 14:40:35 -0300

    up

--- contentScript.js
+++ contentScript.js
@@ -1,20 +1,26 @@
-var line = document.createElement('div');
-line.id = 'line';
-line.style.position = 'fixed';
-line.style.top = '0';
-line.style.left = '0';
-line.style.width = '100%';
-line.style.height = '1px';
-line.style.backgroundColor = 'rgba(0, 0, 0, 0.3)';
-line.style.pointerEvents = 'none';
-line.style.display = 'none';
-document.body.appendChild(line);
+// Function to create the line element
+function createLine() {
+  line = document.createElement('div');
+  line.id = 'line';
+  line.style.position = 'fixed';
+  line.style.top = '0';
+  line.style.left = '0';
+  line.style.width = '100%';
+  line.style.height = '5px';
+  line.style.pointerEvents = 'none';
+  document.body.appendChild(line);
+}
 
-document.addEventListener('mousemove', function (e) {
-  if (line.style.display !== 'none') {
-    line.style.top = e.clientY + 'px';
+// Function to remove the line element from the DOM
+function removeLine() {
+  if (line && line.parentNode) {
+    line.parentNode.removeChild(line);
+    line = null; // Reset line variable
+    chrome.runtime.onMessage.removeListener();
   }
-});
+}
+// Create the line initially
+createLine();
 
 // Listen for messages from the popup script
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
@@ -24,23 +30,28 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     var rgbaColor = hexToRGBA(selectedColor, 0.3); // Convert hex color to RGBA with 0.3 opacity
 
     if (selectedStyle === 'thin') {
-      line.style.height = '1px'; // Thin line
-      line.style.display = 'block'; // Ensure the line is visible
+      line.style.height = '5px'; // Thin line
+      line.style.backgroundColor = rgbaColor; // Apply translucent color to the line
     } else if (selectedStyle === 'thick') {
       line.style.height = '20px'; // Thick line
-      line.style.display = 'block'; // Ensure the line is visible
+      line.style.backgroundColor = rgbaColor; // Apply translucent color to the line
     } else if (selectedStyle === 'disable') {
       // Deactivate the line
-      line.style.display = 'none'; // Hide the line
-      isLineEnabled = false;
+      removeLine();
       return;
     }
-    // Apply translucent color to the line
-    line.style.backgroundColor = rgbaColor;
+    line.style.display = 'block'; // Ensure the line is visible
   }
 });
 
-// Function to convert hex color to RGBA with specified opacity
+// Add event listener to update line position based on mouse movement
+document.addEventListener('mousemove', function (e) {
+  if (line && line.style.display !== 'none') {
+    line.style.top = e.clientY + 'px';
+  }
+});
+
+// // Function to convert hex color to RGBA with specified opacity
 function hexToRGBA(hex, opacity) {
   hex = hex.replace('#', '');
   var r = parseInt(hex.substring(0, 2), 16);
--- popup.js
+++ popup.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
   function disableLine() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {type: 'applyLineStyle', style: 'disable'});
+      chrome.tabs.sendMessage(tabs[0].id, {type: 'disableLine'});
     });
   }
 });
